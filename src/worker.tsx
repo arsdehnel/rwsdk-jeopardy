61d98b881207f1dee1e12966b01c2c7af5c50e69
Adam Dehnel <arsdehnel@gmail.com>
2026-02-27 21:34:38 -0600

    ci: a bunch of automation stuff

--- src/worker.tsx
+++ src/worker.tsx
@@ -1,4 +1,9 @@
 import { render, route } from "rwsdk/router";
+import { env } from "cloudflare:workers";
+import {
+  SyncedStateServer,
+  syncedStateRoutes,
+} from "rwsdk/use-synced-state/worker";
 import { defineApp } from "rwsdk/worker";
 
 import { Document } from "@/app/document";
@@ -6,8 +11,10 @@ import { setCommonHeaders } from "@/app/headers";
 import { Home } from "@/app/pages/home";
 
 export type AppContext = {};
+export { SyncedStateServer };
 
 export default defineApp([
+  ...syncedStateRoutes(() => env.SYNCED_STATE_SERVER),
   setCommonHeaders(),
   ({ ctx }) => {
     // setup ctx here
