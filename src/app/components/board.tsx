0564954ab9bb2f77443dc07c083e57890486a902
Adam Dehnel <arsdehnel@gmail.com>
2026-03-11 13:58:23 -0500

    feat: fix styling of used clues

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -5,7 +5,6 @@ import CategoryColumn from './category-column';
 export default function Board({ categories, usedClueIds }: { categories: Category[]; usedClueIds: string[] }) {
 	return (
 		<div className="jeopardy-board">
-			{usedClueIds.join(',')}
 			{categories.map(category => (
 				<CategoryColumn key={category.id} category={category} usedClueIds={usedClueIds} />
 			))}
