e5f433dc5393277b30ca8d1cf5d398b746270b9a
Adam Dehnel <arsdehnel@gmail.com>
2026-03-28 22:11:11 -0500

    fix: styling of contestant view font sizes

--- src/app/pages/dev/view-contestant.tsx
+++ src/app/pages/dev/view-contestant.tsx
@@ -1,5 +1,6 @@
 'use client';
 import type { RequestInfo } from 'rwsdk/worker';
+import getCategories from '@/categories';
 import ContestantView from '@/views/contestant';
 
 export default function DevViewContestant({ ctx }: RequestInfo) {
@@ -8,7 +9,28 @@ export default function DevViewContestant({ ctx }: RequestInfo) {
 		return <p>Session ID not found, please refresh the page.</p>;
 	}
 
+	const categories = getCategories();
+	const usedClueIds: string[] = [];
+	categories.forEach(cat => {
+		cat.clues.forEach(clue => {
+			if (Math.random() > 0.5) {
+				usedClueIds.push(clue.id);
+			}
+		});
+	});
+
+	const devClueSelected = false; // Set this to true or false to test both states
 	const devIsBuzzedIn = true; // Set this to true or false to test both states
+
+	let selectedClue = null;
+	if (devClueSelected) {
+		selectedClue = {
+			id: 'clue1',
+			value: 200,
+			clue: 'This is a sample clue',
+			response: 'What is a sample clue?',
+		};
+	}
 
 	let buzzerQueue: string[] = [];
 	if (devIsBuzzedIn) {
@@ -18,17 +40,12 @@ export default function DevViewContestant({ ctx }: RequestInfo) {
 	return (
 		<ContestantView
 			selectClue={() => {}}
-			selectedClue={{
-				id: 'clue1',
-				value: 200,
-				clue: 'This is a sample clue',
-				response: 'What is a sample clue?',
-			}}
-			categories={[]}
+			selectedClue={selectedClue}
+			categories={categories}
 			buzzerQueue={buzzerQueue}
 			sessionId={sessionId}
 			buzzIn={() => {}}
-			usedClueIds={[]}
+			usedClueIds={usedClueIds}
 		/>
 	);
 }
