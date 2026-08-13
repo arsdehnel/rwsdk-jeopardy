339215a03602ea043182423ea0832bc196b746cc
Adam Dehnel <arsdehnel@gmail.com>
2026-08-13 07:07:22 -0500

    feat: new play page for separate phase pages
    This isn't wired up yet but it renders and works.

---
+++ src/components/play/clue-overlay.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import type { ClueInGame } from '@/types';
+
+export function ClueOverlay({ selectedClue }: { selectedClue: ClueInGame | null }): React.ReactNode {
+	if (!selectedClue) {
+		return null;
+	}
+
+	return (
+		<div className="clue-overlay">
+			<div className="clue-content">{selectedClue && <p>{selectedClue.text}</p>}</div>
+		</div>
+	);
+}
+
+export default ClueOverlay;
---
+++ src/components/play/clue-select.tsx
@@ -0,0 +1,78 @@
+'use client';
+import { Cross2Icon, RowSpacingIcon } from '@radix-ui/react-icons';
+import { Collapsible } from 'radix-ui';
+import { useState } from 'react';
+import type { CategoryInGame, ClueInGame } from '@/types';
+
+export function ClueSelect({
+	categories,
+	selectClue,
+	usedClueIds,
+	activeContestant,
+	sessionId,
+}: {
+	categories: CategoryInGame[];
+	selectClue: (clue: ClueInGame) => void;
+	usedClueIds: string[];
+	activeContestant: string | undefined;
+	sessionId: string;
+}): React.ReactNode {
+	const [openCategory, setOpenCategory] = useState('');
+
+	const toggleCategory = (categoryId: string): void => {
+		if (openCategory === categoryId) {
+			setOpenCategory('');
+		} else {
+			setOpenCategory(categoryId);
+		}
+	};
+
+	if (activeContestant !== sessionId) {
+		return (
+			<>
+				<h2 className="page-title">Select A Clue</h2>
+				<p>Another player is choosing</p>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<h2 className="page-title">Select A Clue</h2>
+			<div className="clue-select">
+				{categories.map(category => {
+					return (
+						<Collapsible.Root
+							key={category.id}
+							className="CollapsibleRoot"
+							open={openCategory === category.id}
+							onOpenChange={(): void => toggleCategory(category.id)}
+						>
+							<div className="clue-select-category">
+								<h3>{category.name}</h3>
+								<Collapsible.Trigger asChild>
+									<button type="button" className="IconButton">
+										{openCategory === category.id ? <Cross2Icon /> : <RowSpacingIcon />}
+									</button>
+								</Collapsible.Trigger>
+							</div>
+							<Collapsible.Content>
+								<ul className="clue-select-clue-options">
+									{category.clues.map(clue => {
+										return (
+											<li key={clue.id}>
+												<button type="button" onClick={(): void => selectClue(clue)} disabled={usedClueIds.includes(clue.id)}>
+													{clue.value}
+												</button>
+											</li>
+										);
+									})}
+								</ul>
+							</Collapsible.Content>
+						</Collapsible.Root>
+					);
+				})}
+			</div>
+		</>
+	);
+}
