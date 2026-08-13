9c5ad1c505ce5adb7c1139317bfa79587ac2e19d
Adam Dehnel <arsdehnel@gmail.com>
2026-08-13 10:22:30 -0500

    fix: remove temporary logging from session middleware

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -4,8 +4,6 @@ import { sessions } from '@/durable-objects';
 export default async function sessionMiddleware({ ctx, request, response }: RequestInfo<DefaultAppContext>): Promise<void> {
 	try {
 		ctx.session = await sessions.loadFromRequest(request, response.headers);
-		// biome-ignore lint/suspicious/noConsole: temporary exception during debugging new sessions
-		console.log(`CTX session: ${JSON.stringify(ctx.session)}`);
 	} catch (error) {
 		ctx.logger.error(`Uncaught session loading error: ${JSON.stringify(error)}`);
 	}
