748e08944853ba06aa73b14249af88e8f641cb37
Adam Dehnel <arsdehnel@gmail.com>
2026-09-06 09:16:06 -0500

    chore: ignore exports used in same file for knip

--- knip.ts
+++ knip.ts
@@ -1,6 +1,7 @@
 export default {
 	tags: ['-lintignore', '-knipTestExport'],
 	ignoreDependencies: ['cloudflare'],
+	ignoreExportsUsedInFile: true,
 	ignoreFiles: [ 'src/client.tsx' ],
 	compilers: {
 		css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join('\n'),
